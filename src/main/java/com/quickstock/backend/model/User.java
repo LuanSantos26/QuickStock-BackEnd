addda07768e422df7980217dc9a9805d17f8dde6
ItaloShieda <italoshieda@gmail.com>
2026-05-19 15:11:03 -0300

    Adicionando as modificações para conversar com o banco de dados 2.0

--- src/main/java/com/quickstock/backend/model/User.java
+++ src/main/java/com/quickstock/backend/model/User.java
@@ -2,6 +2,7 @@ package com.quickstock.backend.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -11,8 +12,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    private String email;
-    private String password;
-    private String phone;
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String passHash;
+
+    @ManyToOne
+    @JoinColumn(name = "perfil_id", nullable = false)
+    private Perfil perfil;
+
+    @ManyToOne
+    @JoinColumn(name = "empresa_id", nullable = false)
+    private Company empresa;
+
+    @Column(nullable = false)
+    private Short ativo = 1;
+
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    private LocalDateTime criadoEm = LocalDateTime.now();
+
 }
